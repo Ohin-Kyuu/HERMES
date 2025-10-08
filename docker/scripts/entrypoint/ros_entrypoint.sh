e5379fbe61ccd815d6477b261f642f4adf037850
Ohin-Kyuu <ohin.kyuu@gmail.com>
2025-10-08 14:45:49 +0800

    Modified: ros_entrypoint env vars $ROS_WS

--- docker/scripts/entrypoint/ros_entrypoint.sh
+++ docker/scripts/entrypoint/ros_entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 # setup custom workspace environment
-if [ -f "$ROS_WS_PATH/install/setup.bash" ]; then
-  source "$ROS_WS_PATH/install/setup.bash"
+if [ -f "$ROS_WS/install/setup.bash" ]; then
+  source "$ROS_WS/install/setup.bash"
 fi
 exec "$@"
